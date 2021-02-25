2d914eb309113f1036f3701f08f4e5282c27a55f
Kelly Kawase <kellykawase@gmail.com>
2021-02-25 04:58:43 -0300

    Projeto no AspNet Core

---
+++ Projeto/KellyKawase/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KellyKawase.Models;
+
+namespace KellyKawase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PreAgendamento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+                public IActionResult PreAgendamento(Consulta consulta)
+        {
+            ViewBag.agendamento_nome = consulta.nome;
+            ViewBag.agendamento_telefone = consulta.telefone;
+            ViewBag.agendamento_data = consulta.data;
+            ViewBag.agendamento_animal = consulta.animal;
+            ViewBag.agendamento_necessidade = consulta.necessidade;
+            return View("Confirmado");
+
+        }
+
+                public IActionResult Servicos()
+
+        {
+            return View();
+        }
+        public IActionResult Confirmado()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projeto/KellyKawase/Models/Consulta.cs
@@ -0,0 +1,12 @@
+namespace KellyKawase.Models
+{
+    public class Consulta
+{
+    public string nome {get;set;}
+    public string telefone {get;set;}
+    public string data {get;set;}
+    public string animal {get;set;}
+    public string necessidade {get;set;}
+
+}
+}
